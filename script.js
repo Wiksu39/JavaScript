3e1cf689de87bb646069bf4f76438d4d366c7202
Wiksu39 <103591974+Wiksu39@users.noreply.github.com>
2022-05-26 10:52:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,193 @@
+// // let name = 'Jack';
+// // console.log(name);
+// // name = 'Jane';
+// // console.log(name);
+// // let color;
+// // console.log(color);
+// // color = 'red';
+// // console.log(color);
+
+// //JavaScript names can include
+// //letters, numbers, $, _
+
+// //myMainColor1 = 'green';//camal case
+// //my_main_color // snake case
+// //my-main-color // kebab case
+// //MyMainCase // Pascal case
+
+
+// // const name = 'Jack';
+// // console.log(name);
+
+// // const user = {
+// //     name : 'Jack',
+// //     gender: 'male',
+// //     age: '24'
+// // }
+// // console.log(user);
+// // user.name = 'Jane';
+// // console.log(user);
+
+// // const colors =['red', 'orange', 'green'];
+// // console.log(colors);
+// // colors.push('blue');
+// // console.log(colors);
+
+// //String
+
+// const someText = 'Some text';
+// console.log(typeof someText);
+
+// //Number
+
+// const someNumber = 11;
+// console.log(typeof someNumber);
+
+// //Boolean
+
+// const someBoolean = true;
+// console.log(typeof someBoolean);
+
+// //Null
+
+// const someNull = null;
+// console.log(typeof someNull);
+
+// //Undefined
+
+// let someUndefined;
+// console.log(typeof someUndefined);
+
+// //Symbol
+
+// const someSymbol = Symbol ();
+// console.log(typeof someSymbol);
+
+// //Array
+
+// const someArray = [1, 2, 3, 4];
+// console.log(typeof someArray);
+
+// //ObjectLiteral
+
+// const someObjectLiteral = 
+// {
+//     firstProperty: '12',
+//     secondProperty: 'secondProperty'
+// };
+// console.log(typeof someObjectLiteral);
+
+// //Function
+
+// const someFunction = new Function();
+// console.log(typeof someFunction);
+
+// //Date
+
+// const someDate = new Date();
+// console.log(typeof someDate);
+
+// //Conversion to string
+
+// //String
+// let x = String(1112);
+// x = String(2 + 2);
+// x = String(false);
+// x = String(new Date());
+
+// console.log(x);
+// console.log(typeof x);
+// console.log(x.length);
+
+// //toString
+// x =(35).toString();
+// x (true).toString();
+
+// console.log(x);
+// console.log(typeof x);
+// console.log(x.length);
+
+//Conversion to number
+
+// let y =Number('5.1234567');
+// y = Number (false);
+// y = Number ('text');
+// y = Number ([1, 2, 3])
+
+// //parseInt()
+
+// y = parseFloat('123.23');
+// console.log(y);
+// console.log(typeof y);
+// console.log(y.toPrecision(5));
+
+// const x = 5;
+// const y = '3';
+// const z = x + y;
+
+// console.log(z);
+// console.log(typeof z);
+
+// const firstNumber = 50;
+// const secondNumber = 10;
+// let result;
+
+// //Simple math operations
+// result = firstNumber + secondNumber;
+// result = firstNumber - secondNumber;
+// result = firstNumber * secondNumber;
+// result = firstNumber / secondNumber;
+// result = firstNumber % secondNumber;
+
+// //Math object
+// result = Math.PI
+// result = Math.round(3.8);
+// result = Math.ceil(3.3);
+// result = Math.floor(3.7);
+// result = Math.sqrt(9);
+// result = Math.abs(-7);
+// result = Math.pow(2, 2);
+// result = Math.min(3, 4, 5 , 55, 85, -57);
+// result = Math.max(3, 4, 5 , 55, 85, -57);
+// result = Math.random() * 100;
+
+
+// console.log(result);
+
+
+// let result;
+// const firstName = 'Jack';
+// const lastName = 'Brown';
+// const space = ' ';
+// const age = 25;
+// const greeting = "Hey there!";
+
+// //Concatanation
+// result = firstName + space + lastName;
+// result = greeting + space + 'My name is' + space + firstName + ' ' + lastName + ". I'm " + age + " years old.";
+
+// //Escaping
+
+// result = 'I\'m 34 years old';
+// result = "I like \"Star wars\" "
+
+// //Properties and methods
+
+// result = 'Hello!'.length;
+// result = firstName.length;
+
+// result = firstName.concat(space, lastName);
+// result = greeting.concat(space, 'My name is', space, firstName, space, lastName);
+
+// result = result.toLocaleUpperCase();
+// result = result.toLowerCase();
+
+// result = firstName[1];
+// result = 'Hello'.indexOf('l');
+// result = 'Hello lol'.lastIndexOf('l');
+// result = firstName.indexOf('a');
+
+
+// console.log(result);
+
+git status
